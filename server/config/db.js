ff42413b76f35ac3f5b8097f2800f781fb42b868
lkb0398 <lkb0398@nate.com>
2021-11-11 15:30:05 -0500

    change the database name

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = 
 {
-    "URI": "mongodb+srv://techarmy:EXa0jzEH4psTpLU9@techarmy.kqw9d.mongodb.net/techarmy?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://techarmy:785HFu3qQZQoWdPL@techarmysurvey.3dtag.mongodb.net/TechArmySurvey?retryWrites=true&w=majority"
 }
