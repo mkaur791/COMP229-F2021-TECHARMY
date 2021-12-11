0558b421aeb26894dc1bf573a3cb65f546e6c5d8
Manvir Kaur <mkaur791@my.centennialcollege.ca>
2021-12-11 18:44:13 -0500

    change file name to Survey Report

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -141,5 +141,5 @@ const ExportToExcel = (type, fn, dl) => {
     var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
     return dl ?
       XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
-      XLSX.writeFile(wb, fn || ('MySheetName.' + (type || 'xlsx')));
+      XLSX.writeFile(wb, fn || ('SurveyReport.' + (type || 'xlsx')));
  }
